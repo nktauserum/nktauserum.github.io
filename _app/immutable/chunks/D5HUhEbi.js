cfef6ce80371060ecf1cc100a92bf8fd2fc1a077
nktauserum <auserum@proton.me>
2025-08-09 20:25:01 +0300

    Updates

--- _app/immutable/chunks/D5HUhEbi.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_m2zk7p?.base??"",a=globalThis.__sveltekit_m2zk7p?.assets??s;export{a,s as b};
---
+++ _app/immutable/chunks/D767BYQf.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_165tu7k?.base??"",t=globalThis.__sveltekit_165tu7k?.assets??"https://nktauserum.github.io";export{t as a,s as b};

54d3c471f5fbf2759f7f7a896790d2762157a37b
nktauserum <auserum@proton.me>
2025-09-09 18:50:43 +0300

    cleanup unused functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,5 @@
 <script lang="ts">
-	import PostList from '$lib/components/PostList.svelte';
 	import Header from '$lib/components/Header.svelte';
-	import Unauthorized from '$lib/components/Unauthorized.svelte';
-	import { onMount } from 'svelte';
-
-	let is_callback: boolean | null;
-
-	onMount(() => {
-		const params = new URLSearchParams(window.location.search);
-
-		is_callback = params.has('unauthorized');
-	});
 </script>
 
-{#if is_callback}
-	<Unauthorized />
-{/if}
-
 <Header />
-
-<PostList />
